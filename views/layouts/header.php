d35f0a8c476349e8d8762fc0bef03d3ed1c89724
Mikhail Margasov <margasov.mikhail@gmail.com>
2019-06-07 14:12:55 +0300

    Добавил единые страницы успеха отправки формы или неуспеха. Создал папку ассетов. Отправка фалов пока не работает

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -4,10 +4,6 @@
     <meta charset="UTF-8">
     <title><?=$title; ?></title>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <link rel="stylesheet" href="/assets/css/style.css" type="text/css">
 </head>
 <body>
-<script>
-    $(document).ready(function() {
-
-    });
-</script>
